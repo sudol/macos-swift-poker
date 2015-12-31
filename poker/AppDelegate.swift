8521ed8e26a457c73b428eb2588a36d556cb0163
Paul Sudol <psudol@agentisenergy.com>
2015-12-31 13:33:30 -0600

    Initial Commit

---
+++ poker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  poker
+//
+//  Created by Paul Sudol on 12/31/15.
+//  Copyright © 2015 Paul Sudol. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ poker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  poker
+//
+//  Created by Paul Sudol on 12/31/15.
+//  Copyright © 2015 Paul Sudol. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
